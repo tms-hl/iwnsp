2b674c9d04db51ec8ee885f6cad5b8963969aa4f
Nikolaj Tchetchik <nikolaj.tchetchik@tms-hl.org>
2024-01-18 15:19:20 +0100

    gamestate

--- src/gamestate/t1.py
+++ src/gamestate/t1.py
@@ -1,16 +1,16 @@
 
 class Gamestate:
-    def __init__(self, current_level, next_level, level_changed):
+    def __init__(self, current_level, next_level):
         self.current_level = current_level
         self.next_level = next_level 
-        self.level_changed = level_changed
-
-level_changed = property(Gamestate)
     
 current_level = 1
 next_level = 1
 
-print(current_level)
+def lc(self):
+    if current_level != next_level:
+        level_changed = True
+    else:
+        level_changed = False
 
-if current_level != next_level:
-    level_changed = true
+level_changed = property(lc)
