3543c372a9dc808a6782e10c25645e2cedaf7802
Daniel Klingebiel <daniel.klingebiel@tms-hl.org>
2024-01-18 14:51:38 +0100

    events added

--- src/tms_ge/event.py
+++ src/tms_ge/event.py
@@ -1,3 +1,22 @@
-KEYDOWN = 1
-KEYUP = 1
-MOVE
+from enum import Enum
+
+class EventType:
+    USER = 0
+    KEYDOWN = 1
+    KEYUP = 2
+    MOUSEMOVE = 3
+    MOUSEDOWN = 4
+    MOUSEUP = 5
+    COLLIDE = 6
+
+class Event:
+    pass
+
+class EventUser(Event):
+    event_type = EventType.USER
+
+class EventKeyDown(Event):
+    def __init__(self, key, mod, unicode, scancode):
+        '''
+        '''
+        #TODO: Attribute setzen
